eab45781635464135c4e58772859da1d74ef8b7d
root <houssam.khaled@lau.edu>
2024-12-11 23:02:04 +0000

    new alphabeta

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char m = 'a';
+while(m <= 'z')
+{
+if( m != 'q' && m != 'e')
+{
+putchar(m);
+}
+m++;
+}
+putchar('\n');
+return (0); 
+}
