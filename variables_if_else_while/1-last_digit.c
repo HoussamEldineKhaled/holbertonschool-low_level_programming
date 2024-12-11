b081f894753c24838dd7b0eed206ffb93a75ad0d
root <houssam.khaled@lau.edu>
2024-12-11 20:01:27 +0000

    another iff

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (is < 6 && is != 0)
 	{
-	  printf("Last digit of %d is %c and is less than 6 and not 0", n , is);
+	  printf("Last digit of %d is %c and is less than 6 and not 0\n", n , is);
 	}
 	return (0);
 }
