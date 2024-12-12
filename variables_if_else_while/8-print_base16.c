b13c610459b3cea4856038059d5a48b3b2f82245
root <houssam.khaled@lau.edu>
2024-12-12 00:33:04 +0000

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char m= '0';
+char n = 'a';
+while (m <= '9')
+{
+putchar(m);
+m++;
+}
+while (n <= 'f')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
