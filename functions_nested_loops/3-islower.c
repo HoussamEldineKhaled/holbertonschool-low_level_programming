cec5325d1d40a0e7afd4902bc1a4e980c4e85a31
root <houssam.khaled@lau.edu>
2024-12-12 18:14:48 +0000

    lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  *_islower - check for lower case letters
+ *@parameter: integer ASCII value of a char
+ *Return: Either 0 or 1 dependingon condition
 */
 
 int _islower(int c)
