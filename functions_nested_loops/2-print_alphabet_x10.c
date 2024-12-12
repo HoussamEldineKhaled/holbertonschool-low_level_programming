f01711820afac913d767a5e097524e9b02619107
root <houssam.khaled@lau.edu>
2024-12-12 17:51:03 +0000

    10 loops

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
 
 void print_alphabet_x10(void)
 {
-  int n = 0;
-  while (n < 10)
-  {
-    char m = 'a';
-    while (m <= 'z')
-    {
-      _putchar(m);
-      m++;
-    }
-    _putchar('\n');
-    n++;
-  }
+int n = 0;
+while (n < 10)
+{
+char m = 'a';
+while (m <= 'z')
+{
+_putchar(m);
+m++;
+}
+_putchar('\n');
+n++;
+}
 }
