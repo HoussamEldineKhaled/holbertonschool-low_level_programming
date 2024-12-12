62b5bb789010c63b84016a09e9bfd3bec8d8f2b7
root <houssam.khaled@lau.edu>
2024-12-12 17:13:58 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+
+void print_alphabet()
+{
+char m = 'a';
+while(m <= 'z')
+{
+_putchar(m);
+m++;
+}
+_putchar('\n');
+
+}
