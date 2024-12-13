8dbaaa7845844ba1c205ef602bd96e5cbe8f5c9f
root <houssam.khaled@lau.edu>
2024-12-13 13:21:00 +0000

    check upper case

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check upper case
+ * @c: ASCII input
+ * Return: 1 or 0
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
