aec0fde41f62376f1a3d70eeffd758f75aadf1d1
root <houssam.khaled@lau.edu>
2024-12-13 17:35:07 +0000

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@ int i = 0;
 while (i < size)
 {
 int j = 0;
-while(j < size)
+while (j < size)
 {
 _putchar('#');
 j++;
@@ -17,7 +17,7 @@ j++;
 _putchar('\n');
 i++;
 }
-if(size <= 0)
+if (size <= 0)
 {
 _putchar('\n');
 }
