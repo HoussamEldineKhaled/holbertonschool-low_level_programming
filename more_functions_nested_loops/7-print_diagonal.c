a488aa9d8850ae63d7d5afade92550d868cef56b
root <houssam.khaled@lau.edu>
2024-12-13 15:58:15 +0000

    shape line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - diagonal draw
+ * @n: length
+*/
+void print_diagonal(int n)
+{
+int i = 0;
+while(i < n)
+{
+int j = 0;
+while(j < i)
+{
+_putchar(' ');
+j++;
+}
+_putchar('\\');
+_putchar('\n');
+i++;
+}
+if(n <= 0)
+{
+_putchar('\n');
+}
+   
+}
