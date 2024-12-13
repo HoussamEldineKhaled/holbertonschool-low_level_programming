cd61b961e55bf6ed1d1e873a03bc12535bf9935f
root <houssam.khaled@lau.edu>
2024-12-13 14:33:30 +0000

    more numbs

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,11 @@ while (i < 10)
 int j = 0;
 while (j < 15)
 {
-if(j < 10)
-{
-_putchar('0' + j);
-}
-else
+if (j > 10)
 {
 _putchar('0' + (j / 10));
-_putchar('0' + (j % 10));
 }
+_putchar('0' + (j % 10));
 j++;
 }
 _putchar('\n');
