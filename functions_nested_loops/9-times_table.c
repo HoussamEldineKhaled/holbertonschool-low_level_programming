5051c0e3be03551e1081b104fd55fdbeb69b6482
root <houssam.khaled@lau.edu>
2024-12-13 00:55:12 +0000

    table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_tables - multiply
+*/
+
+void times_table(void)
+{
+int x = 0;
+int y = 0;
+while (x < 10)
+{
+y = 0;
+while (y < 10)
+{
+int z = x * y;
+if (y != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (z < 10)
+{
+_putchar(' ');
+_putchar('0' + z);
+}
+else
+{
+_putchar('0' + (z / 10));
+_putchar('0' + (z % 10));
+}
+y++;
+}
+_putchar('\n');
+x++;
+}
+}
