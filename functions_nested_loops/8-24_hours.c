84fffd3faa4ad80f61dbc8bbb9193475b884cb4a
root <houssam.khaled@lau.edu>
2024-12-13 00:07:42 +0000

    Jack Bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - time
+*/
+void jack_bauer(void)
+{
+int h;
+int m;
+while(h < 24)
+{
+while(m < 60)
+{
+_putchar('0' + (h / 10));
+_putchar('0' + (h % 10));
+_putchar(':');
+_putchar('0' + (m / 10));
+_putchar('0' + (m % 10));
+_putchar('\n');
+m++;
+}
+h++;
+}
+}
