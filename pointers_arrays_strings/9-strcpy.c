715973742116dea62f833e90e52f6fa6397c15ef
root <houssam.khaled@lau.edu>
2024-12-18 21:27:06 +0000

    copy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: copied value
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+char *start = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return start;
+}
