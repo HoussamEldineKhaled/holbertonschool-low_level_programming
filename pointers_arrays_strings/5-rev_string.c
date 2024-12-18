9875df74bd38d68f1062d12c1c0607ca406c684b
root <houssam.khaled@lau.edu>
2024-12-18 13:01:05 +0000

    reversal

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@ void rev_string(char *s)
 {
 int len = 0;
 int i;
-char *first, *last, temp; 
+char *first, *last, temp;
 while (s[len] != '\0')
 {
 len++;
@@ -15,7 +15,7 @@ len++;
 i = 0;
 first = s;
 last = s + len - 1;
-while (i < len/2)
+while (i < len / 2)
 {
 temp = *last;
 *last = *first;
