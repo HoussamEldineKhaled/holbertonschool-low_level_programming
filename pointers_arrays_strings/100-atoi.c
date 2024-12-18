b95d0b13fa43e8498df7e6ad6368f712e7917651
root <houssam.khaled@lau.edu>
2024-12-18 23:22:35 +0000

    char to int converter

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,6 +9,8 @@ int _atoi(char *s)
 int i = 0;
 int sign = 1;
 int result = 0;
+int MAX = 2147483647;
+int MIN = -2147483648;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
@@ -25,17 +27,15 @@ else
 i++;
 }
 }
-while (s[i] != '\0')  
+while (s[i] >= '0' && s[i] <= '9')
 {
-if (s[i] >= '0' && s[i] <= '9')
-{
+
+  if (result > (MAX - (s[i] - '0')) / 10 && sign == -1)
+    {
+      return (MIN);
+    }
 result = result * 10 + (s[i] - '0');
 i++;
-}
-else
-{
-break;
-}
 }
 result = result * sign;
 return (result);
