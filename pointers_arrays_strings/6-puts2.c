5401f7202779352553e45e83318b763266456ff0
root <houssam.khaled@lau.edu>
2024-12-18 13:12:40 +0000

    another put

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - every other
+ * @str: string input
+*/
+void puts2(char *str)
+{
+int i = 0;
+while(str[i] != '\0')
+{
+if(i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
+}
