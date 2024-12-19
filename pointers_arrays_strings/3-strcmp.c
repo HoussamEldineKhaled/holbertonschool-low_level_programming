519f31f3d235b272dfd3d960aa8919a2cc114aac
root <houssam.khaled@lau.edu>
2024-12-19 22:44:11 +0000

    compare

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - comparison
+ * @s1: first str
+ * @s2: second str
+ * Return: difference
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+while(*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+return (*s1 - *s2);
+}
