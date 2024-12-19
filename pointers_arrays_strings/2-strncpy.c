90cbb696cccc79764d08dcd042d4f7cfe9d1c53e
root <houssam.khaled@lau.edu>
2024-12-19 17:46:43 +0000

    copy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copy string to another
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: result
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+char *tmp = dest;
+while (i < n)
+{
+*dest = *src;
+i++;
+src++;
+dest++;
+}
+dest = '\0';
+return (tmp);
+}
