75c3cadbb820cc5ed5702c7b0291897abeb08bc6
root <houssam.khaled@lau.edu>
2024-12-20 18:04:44 +0000

    str chr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+char *_strchr(char *s, char c)
+{
+  int i = 0;
+
+  if(c == '\0')
+    {
+      return (s + i);
+    }
+  while(s[i] != '\0')
+    {
+      if(s[i] == c)
+	{
+	  return (s + i);
+	}
+      i++;
+    }
+  return NULL;
+  
+}
