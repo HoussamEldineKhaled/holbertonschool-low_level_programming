45a20eb253cef1da2f4adeee751076f36790d0db
root <houssam.khaled@lau.edu>
2024-12-20 12:22:43 +0000

    capital

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * cap_string - capital
+ * @n: input
+ * Return: capitalized
+*/
+char *cap_string(char *n)
+{
+int i = 1;
+if (n[0] >= 'a' && n[0] <= 'z')
+{
+n[0] -= 32;
+}  
+while (n[i] != '\0')
+{
+if (n[i] >= 'a' && n[i] <= 'z')
+{
+if (n[i - 1] == ' ' || n[i - 1] == '\t' || n[i - 1] == '\n' || n[i - 1] == ',' || n[i - 1] == ';' || n[i - 1] == '.' || n[i - 1] == '!' || n[i - 1] == '?' || n[i - 1] == '"' || n[i - 1] == '(' || n[i - 1] == ')' || n[i - 1] == '{' || n[i - 1] == '}')
+{
+n[i] -= 32; 
+}
+}
+i++;
+}
+return (n);
+}
