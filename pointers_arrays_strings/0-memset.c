2729edfc50ab0b0f0fdae22c0302a4a310102646
root <houssam.khaled@lau.edu>
2024-12-20 15:19:19 +0000

    memory set

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - change set
+ * @s: input
+ * @b: character
+ * @n: number
+ * Return: output
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n){
+s[i] = b;
+i++;
+}
+return (s);
+}
