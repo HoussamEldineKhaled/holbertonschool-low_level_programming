34f9e19e3e5f85fd652b2a12d9a4ab912fe8949a
root <houssam.khaled@lau.edu>
2024-12-20 18:11:53 +0000

    str chr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,25 @@
 #include "main.h"
 #include <stddef.h>
-
+/**
+ * _strchr - string character
+ * @s: string
+ * @c: character
+ * Return: null or the string
+*/
 char *_strchr(char *s, char c)
 {
-  int i = 0;
-
-  if(c == '\0')
-    {
-      return (s + i);
-    }
-  while(s[i] != '\0')
-    {
-      if(s[i] == c)
-	{
-	  return (s + i);
-	}
-      i++;
-    }
-  return NULL;
-  
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+i++;
+}
+if (c == '\0')
+{
+return (s + i);
+}
+return (NULL);  
 }
