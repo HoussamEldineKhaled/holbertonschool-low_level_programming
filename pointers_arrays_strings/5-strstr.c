63ea4e285fce661990893c3c84984360dc1a03a8
root <houssam.khaled@lau.edu>
2024-12-20 21:28:15 +0000

    needle

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strstr - needle haystack
+ * @haystack: big str
+ * @needle: small str
+ * Return: NULL or needle
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+int j;
+while (needle[i] != '\0')
+{
+j = 0;
+while (haystack[j] != '\0')
+{
+if (needle[i] == haystack[j])
+{
+return (needle + i);
+}
+j++;
+}
+i++;
+}
+
+return (NULL);
+}
