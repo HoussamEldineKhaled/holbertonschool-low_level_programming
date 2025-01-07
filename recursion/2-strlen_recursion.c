0f65029fec20b30b4af69fc6a14646d26c9418ce
root <houssam.khaled@lau.edu>
2025-01-07 16:09:33 +0000

    recursive length

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,12 +2,13 @@
 /**
  * _strlen_recusion - length recursive
  * @s: input char
+ * Return: either 0 or length
 */
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return 0;
+return (0);
 }
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
