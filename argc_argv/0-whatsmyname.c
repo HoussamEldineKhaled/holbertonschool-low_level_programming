a411a148b16bf3b0c3383e893ca37a5cbe59d0a2
root <houssam.khaled@lau.edu>
2025-01-08 16:08:01 +0000

    first argc argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,18 @@
 #include "main.h"
+/**
+ * main - main function
+ * @argc: argv size
+ * @argv: array output
+ * Return: 0
+*/
 int main(int argc __attribute__((unused)), char *argv[])
 {
-  int i = 0;
-  while (argv[0][i])
-    {
-      _putchar(argv[0][i]);
-      i++;
-    }
-  _putchar('\n');
-  return (0);
+int i = 0;
+while (argv[0][i])
+{
+_putchar(argv[0][i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
