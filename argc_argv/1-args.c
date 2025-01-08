129c0b796261dcf29365f8e374e8fb3bcdaa3317
root <houssam.khaled@lau.edu>
2025-01-08 18:57:48 +0000

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: size
+ * @argv: argument
+ * Return: 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+_putchar((argc - 1) + '0');
+_putchar('\n');
+return (0);
+}
