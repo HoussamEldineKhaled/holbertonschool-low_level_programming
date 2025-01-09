687296c93fb5f0c054ebee947a6ab9517e2f5de1
root <houssam.khaled@lau.edu>
2025-01-09 19:32:46 +0000

    copy malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
     {
       length++;
     }
-  strcpy = malloc((length) * sizeof(char));
+  strcpy = malloc((length + 1) * sizeof(char));
   while(i < length)
     {
       strcpy[i] = str[i];
