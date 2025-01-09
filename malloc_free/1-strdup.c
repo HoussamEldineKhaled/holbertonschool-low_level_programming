064e2d677dda32fa6c43ffac5c240f60f4446ea1
root <houssam.khaled@lau.edu>
2025-01-09 20:02:53 +0000

    test2

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,32 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _strdup - duplicate string
+ * @str: string
+ * Return: null or strcpy
+*/
 char *_strdup(char *str)
 {
-  int i = 0;
-  int length = 0;
-  char *strcpy;
-  if (str == NULL)
-    {
-      return (NULL);
-    }
-  while(str[length] != '\0')
-    {
-      length++;
-    }
-  strcpy = malloc((length + 1) * sizeof(char));
-
-  if (strcpy == NULL)
-    {
-      return (NULL);
-    }
-  while(i < length)
-    {
-      strcpy[i] = str[i];
-      i++;
-    }
-  strcpy[length] = '\0';
-
-  return (strcpy);
-  
+int i = 0;
+int length = 0;
+char *strcpy;
+if (str == NULL)
+{
+return (NULL);
+}
+while (str[length] != '\0')
+{
+length++;
+}
+strcpy = malloc((length + 1) * sizeof(char));
+if (strcpy == NULL)
+{
+return (NULL);
+}
+while (i < length)
+{
+strcpy[i] = str[i];
+i++;
+}
+strcpy[length] = '\0';
+return (strcpy);
 }
