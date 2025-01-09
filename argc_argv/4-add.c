fc67651baf7c0efb957af054f756139fc8259217
root <houssam.khaled@lau.edu>
2025-01-09 17:20:39 +0000

    summation

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-bool isNumber(char nb[])
-{
-  
-  return (true);
-}
-
+#include <ctype.h>
 int main(int argc, char *argv[])
 {
-int i = 0;
+int i = 1;
+int j = 0;
 int sum = 0;
 while (i < argc)
 {
-      if (!atoi(argv[i]))
+  j = 0;
+  while (argv[i][j] != '\0')
+    {
+      if (!isdigit(argv[i][j]))
 	{
 	  printf("Error\n");
 	  return (1);
 	}
+      j++;
+    }
       sum += atoi(argv[i]);
       i++;
-    }
-  printf("%d\n", sum);
-  return (0);
+}
+printf("%d\n", sum);
+return (0);
 }
