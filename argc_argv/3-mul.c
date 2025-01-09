e6c74ec85f5e48c71d082dfe9acea08da6cdca5b
root <houssam.khaled@lau.edu>
2025-01-09 08:31:40 +0000

    multiplication

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,8 +1,26 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main method
+ * @argc: size array
+ * @argv: array arguments
+ * Return: 0 or 1
+*/
 int main(int argc, char *argv[])
 {
-  _putchar(argv[1] * argv[2]);
-  _putchar('\n');
-  return (0);
+int x;
+int y;
+int mult;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+mult = x * y;
+_putchar(mult + '0');
+_putchar('\n');
+return (0);
 }
