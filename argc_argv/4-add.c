38425d939940b252554871c370bcf71605da1e3b
root <houssam.khaled@lau.edu>
2025-01-09 17:25:35 +0000

    summation

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+ * main - mian method
+ * @argc: number
+ * @argv: array command
+ * Return: 0 or 1
+*/
 int main(int argc, char *argv[])
 {
 int i = 1;
@@ -8,18 +14,18 @@ int j = 0;
 int sum = 0;
 while (i < argc)
 {
-  j = 0;
-  while (argv[i][j] != '\0')
-    {
-      if (!isdigit(argv[i][j]))
-	{
-	  printf("Error\n");
-	  return (1);
-	}
-      j++;
-    }
-      sum += atoi(argv[i]);
-      i++;
+j = 0;
+while (argv[i][j] != '\0')
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+j++;
+}
+sum += atoi(argv[i]);
+i++;
 }
 printf("%d\n", sum);
 return (0);
