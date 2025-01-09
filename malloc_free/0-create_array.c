3e461b6c5f4ffca20dda9cad0ef31f4f71129d4d
root <houssam.khaled@lau.edu>
2025-01-09 14:11:57 +0000

    array input

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * creat_array - array using malloc
+ * @size: array size
+ * @c: character input
+ * Return: array
+*/
+char *create_array(unsigned int size, char c)
+{
+unsigned int i = 0;
+char *arr = malloc(size * sizeof(char));
+if (size == 0 || arr == NULL)
+{
+return (NULL);
+}
+while (i < size)
+{
+arr[i] = c;
+i++;
+}
+return (arr);  
+}
