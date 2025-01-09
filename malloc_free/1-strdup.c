dea33b8557baa80cbc8587a43178e8acbe3407c5
root <houssam.khaled@lau.edu>
2025-01-09 18:09:20 +0000

    duplication

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,8 +1,27 @@
 #include "main.h"
-
+#include <stdlib.h>
 
 char *_strdup(char *str)
 {
   int i = 0;
+  int length = 0;
+  char *strcpy;
+  if (str == NULL)
+    {
+      return (NULL);
+    }
+  while(str[length] != '\0')
+    {
+      length++;
+    }
+  strcpy = malloc((length + 1) * sizeof(char));
+  while(i < length)
+    {
+      strcpy[i] = str[i];
+      i++;
+    }
+  strcpy[length] = '\0';
+
+  return (strcpy);
   
 }
