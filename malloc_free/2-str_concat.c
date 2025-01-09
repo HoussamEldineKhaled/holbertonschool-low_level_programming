6c509ac64f59eb943158b0427b60956d920a482e
root <houssam.khaled@lau.edu>
2025-01-09 21:59:11 +0000

    to be continued

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+
+char *str_concat(char *s1, char *s2)
+{
+  char *sum;
+  if (s1 == NULL)
+    {
+      return (s2);
+    }
+  if (s2 == NULL)
+    {
+      return (s1);
+    }
+  
+  if (sum == NULL)
+    {
+      return (NULL);
+    }
+  return (sum);
+}
