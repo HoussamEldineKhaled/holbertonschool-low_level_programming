cb1373c84649d01a4f9a6abfc8300d46c825a91a
root <houssam.khaled@lau.edu>
2025-01-10 08:04:24 +0000

    concatenation

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,22 +1,47 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 
 char *str_concat(char *s1, char *s2)
 {
+  int i = 0;
+  int j = 0;
   char *sum;
+  int len1 = 0;
+  int len2 = 0;
+
   if (s1 == NULL)
     {
-      return (s2);
+      s1 = "";
     }
   if (s2 == NULL)
     {
-      return (s1);
+      s2 = "";
     }
-  
+  while (s1[len1] != '\0')
+    {
+      len1++;
+    }
+  while (s1[len2] != '\0')
+    {
+      len2++;
+    }
+  sum = malloc(sizeof(char) * (len1 + len2 + 1));
   if (sum == NULL)
     {
       return (NULL);
     }
+  while (i < len1)
+    {
+      sum[i] = s1[i];
+      i++;
+    }
+  while (j < len2 + 1)
+    {
+      sum[i] = s2[j];
+      j++;
+      i++;
+    }
+  sum[i] = '\0'; 
   return (sum);
 }
