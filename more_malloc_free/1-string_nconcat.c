4a46b2aa34effffea9efc4855fcfef8b5636e739
root <houssam.khaled@lau.edu>
2025-01-10 17:11:55 +0000

    concat2

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+  unsigned int i = 0;
+  unsigned int j = 0;
+  char *sum;
+  unsigned int len1 = 0;
+  unsigned int len2 = 0;
+  if (s1 == NULL)
+    {
+      s1 = "";
+    }
+  if (s2 == NULL)
+    {
+      s2 = "";
+    }
+  while (s1[len1] != '\0')
+    {
+      len1++;
+    }
+  while (s2[len2] != '\0')
+    {
+      len2++;
+    }
+  sum = malloc((len1 + n + 1) * sizeof(char));
+  if (sum == NULL)
+    {
+      return (NULL);
+    }
+  if (n >= len2)
+    {
+      n = len2;
+    }
+  while (i < len1)
+    {
+      sum[i] = s1[i];
+      i++;
+    }
+  while (j < n)
+    {
+      sum[i] = s2[j];
+      j++;
+      i++;
+    }
+  sum[i] = '\0';
+  return (sum);
+}
