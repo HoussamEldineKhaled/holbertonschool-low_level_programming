55849352a91e314415c77a688266f66893992419
root <houssam.khaled@lau.edu>
2025-01-10 17:21:36 +0000

    concat2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenate conditionals
  * @s1: first string
@@ -16,30 +15,18 @@ char *sum;
 unsigned int len1 = 0;
 unsigned int len2 = 0;
 if (s1 == NULL)
-{
-s1 = "";
-}
+  s1 = "";
 if (s2 == NULL)
-{
-s2 = "";
-}
+  s2 = "";
 while (s1[len1] != '\0')
-{
-len1++;
-}
+  len1++;
 while (s2[len2] != '\0')
-{
-len2++;
-}
+  len2++;
 sum = malloc((len1 + n + 1) * sizeof(char));
 if (sum == NULL)
-{
-return (NULL);
-}
+  return (NULL);
 if (n >= len2)
-{
-n = len2;
-}
+  n = len2;
 while (i < len1)
 {
 sum[i] = s1[i];
