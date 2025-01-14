f1791d24eb7f9f47c4c9c0dd2f34a09dd1b83f13
root <houssam.khaled@lau.edu>
2025-01-14 20:15:00 +0000

    initialization

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initialize a dog
+ * @d: dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{      
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
