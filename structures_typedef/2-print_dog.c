d95a0bf8022911e911206f021d99894aa84a6553
root <houssam.khaled@lau.edu>
2025-01-15 19:35:31 +0000

    print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,4 +1,22 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-
+/**
+ * print_dog - dog printing
+ * @d: dog
+*/
+void print_dog(struct dog *d)
+{
+if (d->name == NULL)
+{
+d->name = "(nil)";
+}
+else if (d->owner == NULL)
+{
+d->owner = "(nil)";
+}
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
