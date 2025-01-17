300deae95628517f93399d849084f00b92eec1fe
root <houssam.khaled@lau.edu>
2025-01-17 01:49:56 +0000

    to be continued

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,6 +1,29 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
 
 int main (int argc, char *argv[])
 {
+  int n, m, answer;
+
+  int (*op)(int, int);
+  
+  if (argc != 4)
+    {
+      printf("Error\n");
+      exit(98);
+    }
+
+  n = atoi(argv[1]);
+  m = atoi(argv[3]);
+
+  op = get_op_func(argv[2]);
+
+  if (op == NULL)
+    {
+      printf("Error\n");
+      exit(99);
+    }
+  answer = op(n, m);
+  printf("%d\n", answer);
+  return (0);
 }
