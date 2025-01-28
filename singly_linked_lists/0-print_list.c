be6a88857fc1ddaff9c4fc488df0d15a0b8865c5
root <houssam.khaled@lau.edu>
2025-01-28 17:15:28 +0000

    to be continued

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,8 +1,23 @@
-#include "list.h"
+#include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+
 
 size_t print_list(const list_t *h)
 {
-  
+  int i = 0;
+  size_t numb = 0;
+  if (h[0])
+    {
+      printf("[0] (nil)");
+      return (0);
+    }
+
+  while(h[i])
+    {
+      numb++;
+      i++;
+    }
+  return (numb);
 }
