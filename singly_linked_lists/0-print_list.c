e2ebed2a8bd2a02af2a990f6b3e56c5f24e499a1
root <houssam.khaled@lau.edu>
2025-01-29 17:21:50 +0000

    size linked list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,21 +3,27 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * print_list - print length of a linked list
+ * @h: head of inked list
+ * Return: length
+*/
 
 size_t print_list(const list_t *h)
 {
-  int i = 0;
-  size_t numb = 0;
-  if (h[0])
-    {
-      printf("[0] (nil)");
-      return (0);
-    }
-
-  while(h[i])
-    {
-      numb++;
-      i++;
-    }
-  return (numb);
+size_t numb = 0;
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+numb++;
+h = h->next;
+}
+return (numb);
 }
