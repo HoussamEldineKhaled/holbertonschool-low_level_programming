eb555b1ba147085fceddc633dfc21ba58b86ac45
root <houssam.khaled@lau.edu>
2025-01-30 17:07:56 +0000

    print the list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_dllistint - print list
+ * @h: head of the list
+ * Return: length
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t len = 0;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+len++;
+}
+return (len);
+}
