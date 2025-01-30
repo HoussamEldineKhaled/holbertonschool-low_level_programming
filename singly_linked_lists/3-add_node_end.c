b6b536a3288376f871049ae2b767e12f3e2fc675
root <houssam.khaled@lau.edu>
2025-01-30 13:05:24 +0000

    add element to end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,45 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * add_node_end - add node to the end of the linkedlist
+ * @head: head
+ * @str: string input
+ * Return: null and newNode
+*/
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-  int len = 0;
-  list_t *newNode, *temp;
-  char *newStr;
-  if (str == NULL)
-    {
-      return (NULL);
-    }
-  newStr = strdup(str);
-  if (newStr == NULL)
-    {
-      return (NULL);
-    }
-  while (str[len])
-    {
-      len++;
-    }
-  newNode = malloc(sizeof(list_t));
-  if (newNode == NULL)
-    {
-      free(newNode);
-      return (NULL);
-    }
-  newNode->str = newStr;
-  newNode->len = len;
-  newNode->next = NULL;
-  if (*head == NULL)
-    {
-      *head = newNode;
-    }
-  else
-    {
-      temp = *head;
-      while (temp->next != NULL)
-	{
-	  temp = temp->next;
-	}
-      temp->next = newNode;
-    }
-  return (newNode);
+int len = 0;
+list_t *newNode, *temp;
+char *newStr;
+if (str == NULL)
+{
+return (NULL);
+}
+newStr = strdup(str);
+if (newStr == NULL)
+{
+return (NULL);
+}
+while (str[len])
+{
+len++;
+}
+newNode = malloc(sizeof(list_t));
+if (newNode == NULL)
+{
+free(newNode);
+return (NULL);
+}
+newNode->str = newStr;
+newNode->len = len;
+newNode->next = NULL;
+if (*head == NULL)
+{
+*head = newNode;
+}
+else
+{
+temp = *head;
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
+temp->next = newNode;
+}
+return (newNode);
 }
