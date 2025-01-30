d259ffd4aad42c060ef5ebe618bf3b47507d0e76
root <houssam.khaled@lau.edu>
2025-01-30 18:29:39 +0000

    add to beginning of list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,22 +11,20 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *newNode;
-
-  newNode = malloc(sizeof(dlistint_t));
-
-  if (newNode == NULL)
-    {
-      free(newNode);
-      return (NULL);
-    }
-  newNode->n = n;
-  newNode->next = *head;
-  newNode->prev = NULL;
-  if (*head != NULL)
-    {
-  (*head)->prev = newNode;
-    }
-  *head = newNode;
-  return (newNode);
+dlistint_t *newNode;
+newNode = malloc(sizeof(dlistint_t));
+if (newNode == NULL)
+{
+free(newNode);
+return (NULL);
+}
+newNode->n = n;
+newNode->next = *head;
+newNode->prev = NULL;
+if (*head != NULL)
+{
+(*head)->prev = newNode;
+}
+*head = newNode;
+return (newNode);
 }
