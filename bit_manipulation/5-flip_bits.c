a6c33c25cc26470cfdd83a6f5cc8f1fb3e120014
root <houssam.khaled@lau.edu>
2025-01-31 21:52:57 +0000

    flip

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * flip_bits - flips bits of number
+ * @n: number
+ * @m: other number
+ * Return: result
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor = n ^ m;
+unsigned int count = 0;
+while (xor)
+{
+count += xor & 1;
+xor >>= 1;
+}
+return (count)
+}
