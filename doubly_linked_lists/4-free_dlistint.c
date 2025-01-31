720697a626382ba4b9673567895c554415c82b60
root <houssam.khaled@lau.edu>
2025-01-31 11:56:28 +0000

    free

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -7,12 +7,13 @@
  * free_dlistint - free linked list
  * @head: head of list
 */
+
 void free_dlistint(dlistint_t *head)
 {
 if (head == NULL)
 {
 return;
 }
- free_dlistint(head->next);
+free_dlistint(head->next);
 free(head);
 }
