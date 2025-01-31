dc08fdcf4d6917f775dd7e93a079d8558178db33
root <houssam.khaled@lau.edu>
2025-01-31 11:54:36 +0000

    free

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * free_dlistint - free linked list
+ * @head: head of list
+*/
+void free_dlistint(dlistint_t *head)
+{
+if (head == NULL)
+{
+return;
+}
+ free_dlistint(head->next);
+free(head);
+}
