bc3aee5c7a57dd5b90e5879a4836286a931d6f6f
root <houssam.khaled@lau.edu>
2025-01-31 21:24:25 +0000

    setting a bit

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - setting bits
+ * @n: number input
+ * @index: index of number
+ * Return: -1 or 1
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+ if (index >= (sizeof(*n) * 8))
+ {
+ return (-1);
+ }
+ *n |= (1UL << index);
+ return (1);
+}
