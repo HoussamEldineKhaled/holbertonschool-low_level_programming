dd37e40a1ab2029958a5aa076d096bc33394ae71
root <houssam.khaled@lau.edu>
2025-02-02 22:19:44 +0000

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - create a new file
+ * @filename: name of file
+ * @text_content: content of file
+ * Return: -1 and 1
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, w, len = 0;
+if (filename == NULL)
+{
+return (-1);
+}
+fd = open(filename, O_RDONLY | O_WRONLY | O_TRUNC, 0600);
+if (fd < 0)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+while (text_content != NULL)
+{
+len++;
+}
+w = write(fd, text_content, len);
+if (w < 0)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
