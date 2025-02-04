24126ecfcbe62cdedafb881d5bf8cee696c2f780
root <houssam.khaled@lau.edu>
2025-02-04 10:52:48 +0000

    test 10000

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,22 +8,18 @@ int main (int argc, char *argv[])
   int file_to, file_from;
   ssize_t byte_read, byte_written;
   char buffer[1024];
-
   if (argc != 3)
     {
       dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
       exit(97);
     }
-
   file_from = open(argv[1], O_RDONLY);
   if (file_from < 0)
     {
       dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
       exit(98);
     }
-
   file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
   if (file_to < 0)
     {
       dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -43,13 +39,12 @@ int main (int argc, char *argv[])
       dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
       exit(98);
     }
-    
     if (close(file_from) < 0)
       {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
 	exit(100);
       }
-    if (close(file_to))
+    if (close(file_to) < 0)
       {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
         exit(100);
