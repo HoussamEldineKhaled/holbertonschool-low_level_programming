ba1dac8b1944df723f634584db1757ccc526b82e
root <houssam.khaled@lau.edu>
2025-02-04 09:55:03 +0000

    copy contents of file

---
+++ file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+int main (int argc, char *argv[])
+{
+  int file_to, file_from;
+  ssize_t byte_read, byte_written;
+  char buffer[1024];
+
+  if (argc != 3)
+    {
+      dprintf(STDERR_FILENO, "Usage: cpy file_from file_to\n");
+      exit(97);
+    }
+
+  file_from = open(argv[1], O_RDONLY);
+  if (file_from < 0)
+    {
+      dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+      exit(98);
+    }
+
+  file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+  if (file_to < 0)
+    {
+      dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+      exit(99);
+    }
+  byte_read = read(file_from, buffer, 1024);
+  if (byte_read < 0)
+    {
+      dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+    }
+  while (byte_read > 0)
+    {
+      byte_written = write(file_to, buffer, byte_read);
+      if (byte_written < 0)
+	{
+	  dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	  exit(99);
+	}
+    }
+  close(file_to);
+  close(file_from);
+
+  return (0);
+}
